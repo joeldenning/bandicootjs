30ee918650f12d4a51fe023f57448fdb0edafce8
joeldenning <joeldenning@gmail.com>
2015-03-12 00:08:15 -0700

    design changes to API, shown by the to-do-list example code

--- example/to-do-list/todo.js
+++ example/to-do-list/todo.js
@@ -1,38 +1,43 @@
 coot.Event({
 	where: 'To-do list',
-	action: 'create new item'
+	what: 'create new item',
+	this: {
+		dom: {
+			todoList: 'list<thingToDo>',
+			newItem: 'element<input>'
+		},
+		buildingBlocks: {
+			thingToDo: 'thingToDo'
+		}
+	},
+	types: {
+		thingToDo: {
+			checkbox: 'element<input>',
+			label: 'element<label>'
+		}
+	},
+	services: []
 });
 
 coot.Scenario({
+	outcome: 'add new row to bulleted list',
 	where: 'To-do list',
-	action: 'create new item',
-	when: 'domVariables.newItem.getValue().length > 1',
-	what: 'add new row to bulleted list',
-	how: function(domVariables, domExamples, services) {
-		var newItem = domExamples.thingToDo.create()
-		newItem.checkbox.uncheck()
-		newItem.label.setValue('A new thing to do')
-		domVariables.todoList.add(newItem)
-	},
-
-	domVariables: {
-		todoList: {}
-	},
-	domExamples: {
-		thingToDo: {
-			checkbox: 'input',
-			label: 'label'
-		}
-	},
-	services: [],
-})
+	what: 'create new item',
+	when: 'this.dom.newItem.value.length > 1',
+	how: function() {
+		var newItem = this.buildingBlocks.thingToDo.create();
+		newItem.checkbox.checked = false;
+		newItem.label.innerHTML = this.dom.newItem.value;
+		this.dom.todoList.insert(newItem);
+	}
+});
 
 coot.Scenario({
+	outcome: 'warn the user that the item will not be added to the list',
 	where: 'To-do list',
-	action: 'create new item',
-	what: 'new item name is blank',
-	when: 'domVariables.newItem.getValue().match(/[ \\t\\n]*/)',
+	what: 'create new item',
+	when: 'this.dom.newItem.value.match(/[ \\t\\n]*/)',
 	how: function() {
-		alert('Hello!');
+		alert('I am sorry but you can\'t add an item full of whitespace characters');
 	}
-})
+});
