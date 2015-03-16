91260e04d45b40a88e472b7ee7e86f98f928823c
joeldenning <joeldenning@gmail.com>
2015-03-16 16:35:20 -0700

    Changing from tabs to spaces

--- example/to-do-list/todo.js
+++ example/to-do-list/todo.js
@@ -1,43 +1,43 @@
 coot.Event({
-	where: 'To-do list',
-	what: 'create new item',
-	this: {
-		dom: {
-			todoList: 'list<thingToDo>',
-			newItemUserInput: 'element<input>',
-		},
-		buildingBlocks: {
-			thingToDo: 'thingToDo'
-		}
-	},
-	types: {
-		thingToDo: {
-			checkbox: 'element<input>',
-			label: 'element<label>'
-		}
-	},
-	services: []
+  where: 'To-do list',
+  what: 'create new item',
+  this: {
+    dom: {
+      todoList: 'list<thingToDo>',
+      newItemUserInput: 'element<input>',
+    },
+    buildingBlocks: {
+      thingToDo: 'thingToDo'
+    }
+  },
+  types: {
+    thingToDo: {
+      checkbox: 'element<input>',
+      label: 'element<label>'
+    }
+  },
+  services: []
 });
 
 coot.Scenario({
-	outcome: 'add new row to bulleted list',
-	where: 'To-do list',
-	what: 'create new item',
-	when: 'this.dom.newItemUserInput.value.length > 0',
-	how: function() {
-		var newItem = this.buildingBlocks.thingToDo.cloneDeep();
-		newItem.checkbox.checked = false;
-		newItem.label.innerHTML = this.dom.newItemUserInput.value;
-		this.dom.todoList.push(newItem);
-	}
+  outcome: 'add new row to bulleted list',
+  where: 'To-do list',
+  what: 'create new item',
+  when: 'this.dom.newItemUserInput.value.length > 0',
+  how: function() {
+    var newItem = this.buildingBlocks.thingToDo.cloneDeep();
+    newItem.checkbox.checked = false;
+    newItem.label.innerHTML = this.dom.newItemUserInput.value;
+    this.dom.todoList.push(newItem);
+  }
 });
 
 coot.Scenario({
-	outcome: 'warn the user that the item will not be added to the list',
-	where: 'To-do list',
-	what: 'create new item',
-	when: 'this.dom.newItemUserInput.value.length === 0',
-	how: function() {
-		alert('I am sorry but you can\'t add an item full of whitespace characters');
-	}
+  outcome: 'warn the user that the item will not be added to the list',
+  where: 'To-do list',
+  what: 'create new item',
+  when: 'this.dom.newItemUserInput.value.length === 0',
+  how: function() {
+    alert('I am sorry but you can\'t add an item full of whitespace characters');
+  }
 });
